78532409481d22736f3611611b74c4dc33af50e2
Elizabeth <elizng@umich.edu>
2026-03-23 19:51:13 -0400

    example push

--- chameleon_platformio/src/main_for_running_car.cpp
+++ chameleon_platformio/src/main_for_running_car.cpp
@@ -1,25 +1,25 @@
-// #include "main.hpp"
+#include "main.hpp"
 
-// // ------- Objects -------
-// ReactionOut reaction;
-// Car car;
-// // -----------------------
+// ------- Objects -------
+ReactionOut reaction;
+Car car;
+// -----------------------
 
-// void setup(){
-//     Serial.begin(9600);
-//     pinMode(RELAY_PIN, OUTPUT);
-//     pinMode(VALVE_PIN_IN, INPUT);     
+void setup(){
+    Serial.begin(9600);
+    pinMode(RELAY_PIN, OUTPUT);
+    pinMode(VALVE_PIN_IN, INPUT);     
 
-//     car.time_car_move = -1;
-//     Serial.println("Program Started");
-//     delay(5000); // testing speed
+    car.time_car_move = -1;
+    Serial.println("Program Started");
+    delay(2000); // testing speed
 
-// }
+}
 
-// // loop for testing speed
-// void loop() {
-//     if (car.time_car_move == -1){ 
-//         car.time_car_move = millis();
-//         digitalWrite(RELAY_PIN, HIGH);
-//     }
-// }
+// loop for testing speed
+void loop() {
+    if (car.time_car_move == -1){ 
+        car.time_car_move = millis();
+        digitalWrite(RELAY_PIN, HIGH);
+    }
+}
